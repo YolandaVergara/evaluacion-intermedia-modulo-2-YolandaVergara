06d8afb5837e3c1c160bba70a9ae22b9e7cb1aa8
Yolanda <yv.avila@gmail.com>
2019-11-20 09:05:44 +0100

    add conditional to structure

--- js/main.js
+++ js/main.js
@@ -2,5 +2,26 @@ const randomNumber = Math.ceil(Math.random() * 100);
 
 console.log(randomNumber);
 
+const guessButton = document.querySelector('.js-button');
+
+function guessNumber() {
+    const humanNumber = document.querySelector('.js-human-number');
+    console.logitg(humanNumber.value)
+
+    if (humanNumber.value < 0 || humanNumber.value > 100) {
+        document.querySelector('.result').innerHTML = 'Tienes que poner un número entre 1 y 100';
+    } else if (randomNumber > humanNumber.value) {
+        document.querySelector('.result').innerHTML = 'Tu número es más alto';
+    } else if (randomNumber < humanNumber.value) {
+        document.querySelector('.result').innerHTML = 'Tu número es más bajo';
+    } else {
+        document.querySelector('.result').innerHTML = 'Has acertado!';
+    }
+}
+
+function numAttempts() {
+
+}
 
 
+guessButton.addEventListener('click', guessNumber);
