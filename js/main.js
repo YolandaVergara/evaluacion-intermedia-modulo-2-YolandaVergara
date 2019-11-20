9fee98a45713749023295d6299211b52b6a9d42f
Yolanda <yv.avila@gmail.com>
2019-11-20 10:06:41 +0100

    Add num attempts function

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
+'use strict';
 const randomNumber = Math.ceil(Math.random() * 100);
-
 console.log(randomNumber);
-
 const guessButton = document.querySelector('.js-button');
 
-function guessNumber() {
+function guessNumber(event) {
     const humanNumber = document.querySelector('.js-human-number');
-    console.logitg(humanNumber.value)
+    const result = document.querySelector('.result');
+    event.preventDefault();
 
     if (humanNumber.value < 0 || humanNumber.value > 100) {
-        document.querySelector('.result').innerHTML = 'Tienes que poner un número entre 1 y 100';
+        result.innerHTML = 'Tienes que poner un número entre 1 y 100';
     } else if (randomNumber > humanNumber.value) {
-        document.querySelector('.result').innerHTML = 'Tu número es más alto';
+        result.innerHTML = 'Tu número es más alto';
     } else if (randomNumber < humanNumber.value) {
-        document.querySelector('.result').innerHTML = 'Tu número es más bajo';
+        result.innerHTML = 'Tu número es más bajo';
     } else {
-        document.querySelector('.result').innerHTML = 'Has acertado!';
+        result.innerHTML = 'Has acertado!';
     }
 }
 
-function numAttempts() {
+// let totalAttempts = 0;
+// function numAttempts() {
+//     const counterUp = document.querySelector('.attempts');
+//     totalAttempts += 1;
+//     counterUp.innerHTML = `${totalAttempts}`;
+//     console.log('Hola');
+// }
 
-}
+// function game() {
+//     guessNumber();
+//     numAttempts();
+// }
 
 
 guessButton.addEventListener('click', guessNumber);
