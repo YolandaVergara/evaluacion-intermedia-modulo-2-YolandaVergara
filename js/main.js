f5f4442ac4a01bb27b14cc03820d89ff226a102c
Yolanda <yv.avila@gmail.com>
2020-01-30 22:08:26 +0100

    add styles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 "use strict";
+//función que genera el número
 const randomNumber = Math.ceil(Math.random() * 100);
 const guessButton = document.querySelector(".js-button");
 console.log(randomNumber);
@@ -14,16 +15,24 @@ function guessNumber() {
     result.innerHTML = "Tienes que poner un número entre 1 y 100";
   } else if (randomNumber > humanNumber.value) {
     result.innerHTML = "Tu número es más alto";
+    numAttempts();
   } else if (randomNumber < humanNumber.value) {
     result.innerHTML = "Tu número es más bajo";
+    numAttempts();
   } else {
     result.innerHTML = "Has acertado!";
   }
+
 }
 
 let totalAttempts = 0;
 function numAttempts() {
-  const counterUp = document.querySelector(".attempts");
+  const counterUp = document.querySelector(".js-attempts");
+  const finishGame = document.querySelector(".js-finish");
+  if (totalAttempts === 3) {
+    finishGame.innerHTML = 'Perdiste';
+    totalAttempts = 0;
+  }
   totalAttempts += 1;
   counterUp.innerHTML = `${totalAttempts}`;
 }
@@ -31,7 +40,7 @@ function numAttempts() {
 function game(event) {
   event.preventDefault();
   guessNumber();
-  numAttempts();
+
 }
 
 guessButton.addEventListener("click", game);
